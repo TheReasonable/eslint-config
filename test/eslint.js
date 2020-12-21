516c57e83f0940facf1cc6254772fded3bf62140
Nikita Karamov <nick@karamoff.dev>
2020-12-21 14:25:10 +0300

    Add testing apart from linting

---
+++ test/eslint.js
@@ -0,0 +1,29 @@
+const assert = require('assert').strict;
+const { ESLint } = require('eslint');
+const { test } = require('uvu');
+
+// cred: Standard
+// https://github.com/standard/eslint-config-standard/blob/8c5203f7e48177345a7aaf212cb0cb5915880096/test/validate-config.js#L4
+test('ESLint config is valid', async () => {
+	const cli = new ESLint({
+		useEslintrc: false,
+		baseConfig: require('../index'),
+	});
+
+	// basic code example that should pass the tests
+	const code = `const foo = 0;
+const bar = {
+	baz: 2,
+	quux: 222,
+};
+bar
+	.baz = 4;
+`;
+
+	assert.equal(
+		(await cli.lintText(code))[0].errorCount,
+		0,
+	);
+});
+
+test.run();
