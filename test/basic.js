6d13bd46045002bc6ba87424bb73920d6ba425ee
Nikita Karamov <nick@karamoff.dev>
2021-03-23 11:45:46 +0100

    Update other JS files in accordance to the new rules

--- test/basic.js
+++ test/basic.js
@@ -1,22 +1,22 @@
-const assert = require('assert');
-const { test } = require('uvu');
+const assert = require("assert");
+const { test } = require("uvu");
 
 // cred: Airbnb
 // https://github.com/airbnb/javascript/blob/63098cbb6c05376dbefc9a91351f5727540c1ce1/packages/eslint-config-airbnb-base/test/requires.js#L7
-test('require does not throw', () => {
-	assert.doesNotThrow(() => require('../index'));
+test("require does not throw", () => {
+	assert.doesNotThrow(() => require("../index"));
 });
 
 // cred: Standard
 // https://github.com/standard/eslint-config-standard/blob/8c5203f7e48177345a7aaf212cb0cb5915880096/test/basic.js#L4
-test('config fields are objects', () => {
-	const config = require('../index');
+test("config fields are objects", () => {
+	const config = require("../index");
 	[
-		'env',
-		'parserOptions',
-		'rules',
+		"env",
+		"parserOptions",
+		"rules",
 	].forEach((field) => {
-		assert.ok(typeof config[field] === 'object' && config[field] !== null);
+		assert.ok(typeof config[field] === "object" && config[field] !== null);
 	});
 });
 
